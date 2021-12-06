a85803936d68cdfd10de19a7901af297b89a6e0b
haikalgnz <haikalofc8@gmail.com>
2021-12-07 06:40:35 +0700

    ikalbotz

---
+++ shop/allpayment.js
@@ -0,0 +1,19 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY IKAL BOTZ
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0853 6381 0820 
+2. PULSA 
+	ㅁ 0896 0304 2257
+3. DANA
+	ㅁ 0896 0304 2257
+	
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,17 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0853 6381 0820 
+2. PULSA 
+	ㅁ 0896 0304 2257 
+3. DANA
+	ㅁ 0896 0304 2257 
+	
+`
+	}
+
+exports.donasibot = donasibot
+ 
